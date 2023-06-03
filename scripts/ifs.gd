7ff64dbf60f5ea910718293c04071f80c7e73097
Anomasie <umdieeckedenker@gmail.com>
2023-06-03 14:20:06 +0200

    non-equal distribution

--- scripts/ifs.gd
+++ scripts/ifs.gd
@@ -4,12 +4,21 @@ var systems = [] # array of contractions
 
 func random_walk(pos, length=1, distribution=[]):
 	if length > 0:
-		pos = systems[ randi_range(0, len(systems)-1) ].apply(pos)
-		return random_walk(
-			pos,
-			length - 1,
-			distribution
-		)
+		if len(distribution) == 0:
+			return random_walk(
+				systems[ randi_range(0, len(systems)-1) ].apply(pos),
+				length - 1,
+				distribution
+			)
+		else:
+			var random = randf_range(0, distribution[-1])
+			for i in len(distribution):
+				if random <= distribution[i]:
+					return random_walk(
+						systems[ i ].apply(pos),
+						length - 1,
+						distribution
+					)
 	else:
 		return pos
 
@@ -22,10 +31,20 @@ func calculate_fractal(start=Vector2(0.5,0.5), delay=10, points=10000):
 		start = random_walk(start, delay)
 		# real points
 		result.append(start)
+		var distribution = get_distribution()
 		for n in range(1, points+1):
 			result.append( random_walk(
 				result[n-1],
 				1,
-				[]
+				distribution
 			) )
 	return result
+
+func get_distribution():
+	var sum = 0.0
+	var distribution = []
+	for function in systems:
+		sum += function.contract.x * function.contract.y
+		distribution.append(sum)
+	print(distribution)
+	return distribution
