c089c0ea7fdc6e58660a3a6a532a13dd9cf7e2be
Anomasie <umdieeckedenker@gmail.com>
2024-02-03 10:28:22 +0100

    updated minimal sizes

--- scenes/GUI.gd
+++ scenes/GUI.gd
@@ -4,6 +4,7 @@ extends Control
 @onready var PlaygroundUI = $Lines/Content/PlaygroundUI
 @onready var ResultUI = $Lines/Content/ResultUI
 @onready var ResizeTimer = $Lines/Content/ResizeTimer
+@onready var Title = $Lines/Title
 
 func _ready():
 	get_viewport().connect("size_changed", _on_viewport_resize)
@@ -16,14 +17,16 @@ func _on_viewport_resize():
 	# get new size of viewport
 	var viewport_size = get_viewport().get_size()
 	# landscape format or portrait format?
+	## landscape format
 	if viewport_size.x >= viewport_size.y:
 		Content.columns = 2
 		# set minimal window size
-		DisplayServer.window_set_min_size(Vector2i(900, 400))
+		DisplayServer.window_set_min_size(Vector2i(900, 420))
 		# get maximal size of unitary sqare
 		## -2*16: for seperation
 		## in x-axis: -2*32 because of ui elements
 		Global.LOUPE = min(viewport_size.x / 2 - 2 * 16 - 2 * 64, viewport_size.y - 4 * 16 - 160) * Vector2i(1,1)
+	## portrait format
 	else:
 		Content.columns = 1
 		# set minimal window size
@@ -32,7 +35,7 @@ func _on_viewport_resize():
 		## -2*16: for seperation
 		## in x-axis: -2*64 because of buttons
 		## in y-axis: -2*64 because of sliders
-		Global.LOUPE = min(viewport_size.x - 4 * 16 - 2 * 64, viewport_size.y / 2 - 4 * 16 - 80) * Vector2i(1,1)
+		Global.LOUPE = min(viewport_size.x - 4 * 16 - 2 * 64, viewport_size.y / 2 - 4 * 16 - 80 - Title.size.y) * Vector2i(1,1)
 	if Global.LOUPE.x < 1:
 		Global.LOUPE.x = 1
 	if Global.LOUPE.y < 1:
@@ -42,8 +45,6 @@ func _on_viewport_resize():
 	ResultUI.resize()
 	# prevent resizing-bugs
 	ResizeTimer.start()
-	
-	print(viewport_size)
 
 func show_results(results):
 	ResultUI.open(results)
