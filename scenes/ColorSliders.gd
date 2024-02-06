b87a10efbf173658df2531df12f36e559ad864fc
Anomasie <umdieeckedenker@gmail.com>
2024-02-06 09:37:06 +0100

    added html-line-edit to new color picker (which is still unused)

--- scenes/ColorSliders.gd
+++ scenes/ColorSliders.gd
@@ -13,13 +13,16 @@ extends MarginContainer
 @onready var UserPresetNode = $Content/Columns/Sliders/UserPresets
 @onready var UserPresets = $Content/Columns/Sliders/UserPresets.get_children()
 
-var saved_colors = [Color.MIDNIGHT_BLUE, Color.WEB_GRAY, Color.ANTIQUE_WHITE, Color.CRIMSON, Color.GOLD, Color.LIME_GREEN, Color.AQUA, Color.ROYAL_BLUE]
+@onready var Hash = $Content/Columns/Sliders/Presets/Hash
+
+var saved_colors = [Color.CRIMSON, Color.GOLD, Color.LIME_GREEN, Color.ROYAL_BLUE]
 var user_saved_colors = []
 
 func _ready():
 	# connect preset-colors
 	for i in len(Presets):
-		Presets[i].pressed.connect(_on_preset_pressed.bind(i))
+		if Presets[i] is TextureButton:
+			Presets[i].pressed.connect(_on_preset_pressed.bind(i))
 	for i in len(UserPresets):
 		UserPresets[i].pressed.connect(_on_user_preset_pressed.bind(i))
 	# load ui
@@ -29,7 +32,8 @@ func _ready():
 
 func load_preset_colors():
 	for i in len(Presets):
-		Presets[i].modulate = saved_colors[ min(len(saved_colors) - 1,  i) ]
+		if Presets[i] is TextureButton:
+			Presets[i].modulate = saved_colors[ min(len(saved_colors) - 1,  i) ]
 
 func load_user_preset_colors():
 	for i in len(UserPresets):
@@ -68,6 +72,8 @@ func set_color(color = get_color()):
 	if color.v > 0:
 		SatSlider.value = color.s * SatSlider.max_value
 	ValueSlider.value = color.v * ValueSlider.max_value
+	# update hash
+	delete_hash_text()
 
 func get_color():
 	return Color.from_hsv(
@@ -91,3 +97,19 @@ func _on_add_button_pressed():
 	while len(user_saved_colors) > len(UserPresets):
 		user_saved_colors.pop_back()
 	load_user_preset_colors()
+
+# html text
+
+func delete_hash_text():
+	Hash.text = ""
+	Hash.placeholder_text = Preview.modulate.to_html(false)
+
+func _on_hash_text_submitted(new_text):
+	Hash.release_focus()
+	if len(new_text) == 6:
+		set_color(Color.from_string(new_text, Preview.modulate))
+	else:
+		delete_hash_text()
+
+func _on_hash_focus_exited():
+	_on_hash_text_submitted(Hash.text)
