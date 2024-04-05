fddeec2cc116d1594ca97b50c9514b6158e1e155
Anomasie <umdieeckedenker@gmail.com>
2024-04-05 10:02:58 +0200

    re-introduced rounding-button

--- scenes/advanced-options/JsonEdit.gd
+++ scenes/advanced-options/JsonEdit.gd
@@ -3,17 +3,25 @@ extends CodeEdit
 signal please_reload
 
 const DIGITS = 0.05
-const MATH_SYMBOLS = [" ", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ".", ":", "="]
-
-var length_text = 0
-var bg_input_pos = [-1,-1]
-var delay_input_pos = [-1,-1]
 
 @onready var ReloadButton = $ReloadButton
 @onready var RoundButton = $RoundButton
 
 func do_i_have_focus():
 	return self.has_focus() or RoundButton.has_focus()
+
+func get_dict_from_text():
+	# read text -> json
+	var json = JSON.new()
+	var error = json.parse(text)
+	var dict = {}
+	if error == OK:
+		dict = json.get_data()
+		if typeof(dict) != TYPE_DICTIONARY:
+			print("Error in PythonEdit, read_text: Unexpected data type of text")
+	else:
+		print("Error in PythonEdit, read_text: JSON Parse Error: ", json.get_error_message(), " in text at line ", json.get_error_line())
+	return dict
 
 func load_text(ifs):
 	# ifs -> dict
@@ -26,22 +34,34 @@ func load_text(ifs):
 	text = dict_string
 
 func read_text():
-	# read text -> json
-	var json = JSON.new()
-	var error = json.parse(text)
-	var dict = {}
-	if error == OK:
-		dict = json.get_data()
-		if typeof(dict) != TYPE_DICTIONARY:
-			print("Error in PythonEdit, read_text: Unexpected data type of text")
-	else:
-		print("Error in PythonEdit, read_text: JSON Parse Error: ", json.get_error_message(), " in text at line ", json.get_error_line())
+	var dict = get_dict_from_text()
 	# read dict -> ifs
 	var ifs = IFS.from_dict(dict)
 	return ifs
 
 func round_numbers():
-	text = "rounded numbers"
+	# get dict
+	var dict = get_dict_from_text()
+	# round
+	if dict.has("systems") and typeof(dict["systems"]) == TYPE_ARRAY:
+		for system in dict["systems"]:
+			# translation
+			if system.has("translation") and typeof(system["translation"]) == TYPE_ARRAY and len(system["translation"]) == 2:
+				system["translation"] = [
+					snapped(system["translation"][0], DIGITS),
+					snapped(system["translation"][1], DIGITS)
+				]
+			# matrix
+			if system.has("matrix") and typeof(system["matrix"]) == TYPE_ARRAY and len(system["matrix"]) == 4:
+				system["matrix"] = [
+					snapped(system["matrix"][0], DIGITS),
+					snapped(system["matrix"][1], DIGITS),
+					snapped(system["matrix"][2], DIGITS),
+					snapped(system["matrix"][3], DIGITS)
+				]
+	# set text
+	text = JSON.stringify(dict, "\t")
+	# emit signal
 	text_changed.emit()
 
 # signals
